e8688b7774b00fffe65200c7b1f14d023f8aeadb
simonmonk <srmonk@gmail.com>
2025-05-09 07:32:43 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# metamorphopsia
-An open Python tool for assessing Metamorphopsia caused by an epiretinal membrane
+# metamorphopsia.py
+
+An open Python tool for assessing Metamorphopsia caused by an epiretinal membrane (ERM)
+
+I have an ERM in my right eye, and at the end of a recent examination, I was given an Amsler chart to take home, to use for self assessment of the amount of distortion. With the the words *if it gets worse, come back and see us. If not, see you in a year*
+
+The *Amsler Recording Chart*, is just an A5 sheet of paper with a 5mm pitch grid printed on it and a dot in the middle. When you look at it with one eye (staring at the dot) you see distortion in the grid. For me, the horizontal lines bend near the middle.
+
+On the way home from the opticians, I started thinking about how this self-assessment could be done in a more quantitative manner.
+
+The result of this is a prototype software tool that I can use to record my metamorphopsia. 
+
+# Installation
+
+The code is pure Python 3, and needs the following modules, that I think are part of most Python distributions:
+
+* Tkinter
+* PIL
+
+I have only tested it on Mac OS. But it should be fine on any platform.
+
+# Use
+
+The program is pretty simple, all contained in a single Python file. You can run it like this:
+
+```
+python3 metamorphopsia.py
+```
+
+This will open a window looking like this.
+
+![The undistorted grid](figs/fig1.png)
+
+The process with my software tool is, while looking at the grid with the eye you want to assess (let's call this the *bad eye*), adjust the horizontal lines until they appear straight, with the bad eye. In doing this, you will have distorted the grid, to compensate for the distortion in the bad eye. When you look at the grid with your good eye, you will see the distorted grid, that you can save as a record.
+
+Here's the grid that looks regular for me using my bad eye.
+
+![Simon's right eye distortion](figs/fig2.png)
+
+In more detail, here is the procedure.
+
+* Closing your bad eye, look at the dot in the centre of the grid (the grid lines look bumpy). Try and keep a consistent distance to the window.
+* Use your mouse (or get someone else to do this) to drag the middle line on which the red dot is placed so that it is flat.
+* You may find that while the middle of the distortion is corrected, the corrective bump is not wide enough. If so, adjust the *Bump Width* slider, until the middle line is as flat as possible. All the time, try and focus on the dot.
+* Now adjust the lines above and below the middle line, to make them flat, always coming back to focus on the dot.
+
+That's it. You can now save the chart, from the *File* menu.
+
+![Menu](figs/fig3.png)
+
+Saving, saves the chart as data, so, you can if you want re-open it and edit an old chart, before saving it again. 
+
+The *Export...* menu option allows you to save the plot as a PNG image.
+
+# Caveat
+
+I have zero optometry training. I can barely spell it. So please, please, please **DO NOT RELY ON THIS TOOL**. Go and see a professional.
+
+I am very aware that this works for me, as my distortions are only along horizontal lines. And essentially I have a single distortion 'bump', that extends to several lines above and below the dot. And, imaging the distortion may be on different axes for different cases.
+
+Assuming the distortion is always limited to one plane, it would be relatively easy to add a *rotation* feature to the grid.
+
+I'd love to enter into a discussion on this with an optometrist, and welcome pull requests for improvements.
+
---
+++ metamorphopsia.py
@@ -0,0 +1,159 @@
+from tkinter import * 
+from tkinter.filedialog import asksaveasfile, askopenfile
+from datetime import datetime
+import json
+from PIL import Image
+import os
+
+num_lines = 21
+dx = 30 # pixels
+width = (num_lines-1) * dx # pixels
+mid_x = int(num_lines / 2) * dx
+dot_r = dx / 10 # pixels
+bump_r = 2 * dx
+date = datetime.now().strftime("%Y-%m-%d")
+title_base = "Metamorphopsia Measurement Tool"
+
+selected_line_num = None
+
+window = Tk() 
+window.title('Metamorphopsia Measurement Tool') 
+window.geometry(str(width) + "x" + str(width + 200)) 
+
+# offset and value units of 1/10 of dx
+h_lines = [ {'offset': 0, 'value': 0} for x in range(num_lines)]
+
+canvas = Canvas(window, bg="white", height=width, width=width) 
+button_frame = Frame(window)
+
+def set_window_title():
+    title = title_base + " (" + date + ")"
+    window.title(title)
+
+def reset():
+    global h_lines, v_lines
+    h_lines = [ {'offset': 0, 'value': 0} for x in range(num_lines)]     
+    draw_lines()    
+
+def save_as():
+    now = datetime.now() # current date and time
+    timestamp_str = now.strftime("%Y-%m-%d")
+    file = asksaveasfile(initialfile = timestamp_str,
+        initialdir='.',
+        defaultextension=".met",
+        filetypes=[("Metamorphopsia JSON","*.met")])
+    data = {'bump_r': bump_r, 'h_lines': h_lines, 'date': date}
+    if file is None: # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    json.dump(data, file)
+    file.close()
+    
+
+def open_file():
+    global h_lines, bump_r, date
+    file = askopenfile(initialdir='.',
+        defaultextension=".met",
+        filetypes=[("Metamorphopsia JSON","*.met")])
+    if file is None: # return `None` if dialog closed with "cancel".
+        return
+    data = json.load(file)
+    bump_r = data['bump_r']
+    h_lines = data['h_lines']
+    date = data['date']
+    draw_lines()
+    file.close()    
+    set_window_title()
+
+def export_image():
+    now = datetime.now() # current date and time
+    timestamp_str = now.strftime("%Y-%m-%d")
+    file = asksaveasfile(initialfile = timestamp_str,
+        initialdir='.',
+        defaultextension=".png",
+        filetypes=[("PNG","*.png")])
+    if file is None: # return `None` if dialog closed with "cancel".
+        return
+    canvas.postscript(file=file.name + '.eps')
+    img = Image.open(file.name + '.eps') 
+    img.save(file.name, 'png') 
+    os.remove(file.name + '.eps')
+
+def draw_lines():
+    canvas.delete('all')
+    for h_line in range(num_lines-1):
+        draw_h_line(h_line)
+    for v_line in range(num_lines-1):
+        draw_v_line(v_line)
+    draw_dot()
+
+def draw_dot():
+    mid_x_line = h_lines[10]
+    crossing_x = mid_x
+    crossing_y = mid_x
+    canvas.create_oval(crossing_x-dot_r, crossing_y-dot_r, crossing_x+dot_r, crossing_y+dot_r, fill="red")
+
+def draw_h_line(line_num):
+    y = line_num * dx
+    line = h_lines[line_num]
+    left = (0, y)
+    right = (width-1, y)
+    distortion_0 = ((mid_x + line['offset'] - bump_r), y)
+    distortion = ((mid_x + line['offset']), (y + line['value']))
+    distortion_1 = ((mid_x + line['offset'] + bump_r), y)
+    color = 'black'
+    if selected_line_num == line_num:
+        color = 'blue'
+    canvas.create_line(left, distortion_0, distortion, distortion_1, right, fill=color, smooth="bezier")
+
+def draw_v_line(line_num):
+    canvas.create_line(line_num * dx, 0, line_num * dx, width-1)
+
+def click(event):
+    global selected_line_num
+    x, y = event.x, event.y
+    #print('{}, {}'.format(x, y))
+    selected_line_num = round(y / dx)
+
+def motion(event):
+    if not selected_line_num:
+        return
+    selected_line = h_lines[selected_line_num]
+    x, y = event.x, event.y
+    x_offset_pixels = x - mid_x
+    y_offset_pixels = y - (selected_line_num * dx)
+    selected_line['offset'] = x_offset_pixels
+    selected_line['value'] = y_offset_pixels
+    draw_lines()
+
+canvas.bind('<B1-Motion>', motion)
+canvas.bind('<Button-1>', click)
+
+def changed_bump(w):
+    global bump_r
+    bump_r = int(w)
+    draw_lines()
+
+width_label = Label(button_frame, text="Bump Width")
+width_label.pack(side='left')
+
+bump_width_scale = Scale(button_frame, from_=dx, to=dx*5, command=changed_bump, orient=HORIZONTAL)
+bump_width_scale.pack(side='left')
+
+reset_button = Button(button_frame, command = reset, height = 2,  width = 10, text = "Reset")
+reset_button.pack(side='left')
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="Reset", command=reset)
+filemenu.add_command(label="Open...", command=open_file)
+filemenu.add_command(label="Save as...", command=save_as)
+filemenu.add_command(label="Export Image...", command=export_image)
+
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+window.config(menu=menubar)
+draw_lines()
+canvas.pack()
+button_frame.pack(side='top', fill='x')
+window.mainloop() 
